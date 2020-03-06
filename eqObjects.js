c444131bbcac96cc57443d62bdff5fe8435d082d
Ahmed Alwardani <aalwa049@gmail.com>
2020-03-06 16:21:40 -0500

    implemented recursion in eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -22,18 +22,26 @@ const eqObjects = function(object1, object2) {
   let object1Keys = Object.keys(object1);
   let object2Keys = Object.keys(object2);
   let result = object1Keys.length === object2Keys.length;
-
+  
   for (const key of object1Keys) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!(eqArrays(object1[key], object2[key]))) {
         result = false;
       }
+    } else if (typeof object1[key] === "object" && typeof object2[key] === "object") {
+      result = eqObjects(object1[key], object2[key]);
     } else if (object1[key] !== object2[key]) {
       result = false;
     }
   }
   return result;
 };
+
+console.log(assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)); // => true
+console.log(assertEqual(eqObjects({ a: { s: 1, q: 2 }}, {  s: 1, q: 2 }), false)); // => true
+console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2, c: "hi" }, { a: { z: 1 }, b: 2 }), false)); // => false
+
+
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
@@ -45,7 +53,7 @@ const abc = { a: "1", b: "2", c: "3"};
 
 const students = { a: ["Ali", "Ahmed"], s: "steven", c: "1", d: ["2", 3] };
 const dc = { s: "steven", c: "1", d: ["2", 3], a: ["Ali", "Ahmed"] };
-console.log(assertEqual(eqObjects(students, dc), true));
+// console.log(assertEqual(eqObjects(students, dc), true));
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 // console.log(assertEqual(eqObjects(cd, cd2), true));
