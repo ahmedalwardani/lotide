94b0aacb60d937906d742e141d74a31529f90717
ahmedalwardani <aalwa049@uottawa.ca>
2020-08-13 19:56:56 -0400

    comment eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,18 +1,19 @@
 const eqArrays = require("./eqArrays");
+const assertEqual = require("./assertEqual");
 
 const eqObjects = function(object1, object2) {
-  let object1Keys = Object.keys(object1);
-  let object2Keys = Object.keys(object2);
-  let result = object1Keys.length === object2Keys.length;
+  let object1Keys = Object.keys(object1); //returns [an array of object1 keys]
+  let object2Keys = Object.keys(object2); //returns [an array of object2 keys]
+  let result = object1Keys.length === object2Keys.length; //check first if both objects have the same number of key-value pairs
   
   for (const key of object1Keys) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!(eqArrays(object1[key], object2[key]))) {
-        result = false;
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { //if the value of the key is an array in both objects (both values in the two objects at that key are arrays)
+      if (!(eqArrays(object1[key], object2[key]))) { //check if the arrays are not equal
+        result = false; //if they are not equal, no need to continue
       }
-    } else if (typeof object1[key] === "object" && typeof object2[key] === "object") {
-      result = eqObjects(object1[key], object2[key]);
-    } else if (object1[key] !== object2[key]) {
+    } else if (typeof object1[key] === "object" && typeof object2[key] === "object") { //if the value of the key is an object in both objects (both values in the two objects at that key are objects)
+      result = eqObjects(object1[key], object2[key]); //call the same function again using the appropriate objects
+    } else if (object1[key] !== object2[key]) { //if the values are primitive values that are not equal
       result = false;
     }
   }
@@ -20,3 +21,5 @@ const eqObjects = function(object1, object2) {
 };
 
 module.exports = eqObjects;
+
+
